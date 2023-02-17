dbc550dd1765feead833b294369e589c256fb8b6
Arthur bergere <artesburgerking@gmail.com>
2023-02-17 14:27:08 +0100

    [FUNC] Moder ress, que si l'utilisateur a la role 3 (à changer)

--- ApiCube/ApiCube/Models/BuisnessObjects/Utilisateur.cs
+++ ApiCube/ApiCube/Models/BuisnessObjects/Utilisateur.cs
@@ -25,10 +25,41 @@ namespace ApiCube.Models.BuisnessObjects
         public Adresse? Adresse { get; set; }
 
         [NotMapped]
-        public virtual ICollection<ModererCom> ModererComs { get; set; }
+        private ICollection<ModererCom>? _modererComs;
         [NotMapped]
-        public virtual ICollection<ModererRes> ModererRess{ get; set; }
+        public virtual ICollection<ModererCom> ModererComs
+        {
+            get
+            {
+                if (Role == "3")
+                {
+                    return _modererComs ??= new List<ModererCom>();
+                }
+                else
+                {
+                    throw new UnauthorizedAccessException("L'utilisateur n'a pas le rôle requis pour accéder à cette propriété.");
+                }
+            }
+        }
         [NotMapped]
-        public virtual ICollection<Relation> Relations { get; set; }
+        private ICollection<ModererRes>? _modererRess;
+        [NotMapped]
+        public virtual ICollection<ModererRes> ModererRess
+        {
+            get
+            {
+                if (Role == "3")
+                {
+                    return _modererRess ??= new List<ModererRes>();
+                }
+                else
+                {
+                    throw new UnauthorizedAccessException("L'utilisateur n'a pas le rôle requis pour accéder à cette propriété.");
+                }
+            }
+        }
+
+        [NotMapped]
+        public virtual ICollection<Relation>? Relations { get; set; }
     }
 }

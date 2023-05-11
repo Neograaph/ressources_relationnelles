a14df98e2a65b91bcd7f1a12f9b185cfe7cc02e0
neograaph <max45270@gmail.com>
2023-05-11 17:32:11 +0200

    inscription page

--- Front/src/app/Bloc/Authentification/bloc-inscription/bloc-inscription.component.ts
+++ Front/src/app/Bloc/Authentification/bloc-inscription/bloc-inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bloc-inscription',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./bloc-inscription.component.css']
 })
 export class BlocInscriptionComponent {
+  inscriptionForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.inscriptionForm = this.formBuilder.group({
+      nom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submitForm() {
+    if (this.inscriptionForm.valid) {
+      // Traitez les données d'inscription ici, par exemple, appelez un service pour envoyer les données au serveur.
+      console.log(this.inscriptionForm.value);
+    } else {
+      // Affichez des messages d'erreur ou effectuez d'autres actions appropriées si le formulaire n'est pas valide.
+      console.log('Le formulaire d\'inscription est invalide.');
+    }
+  }
 }

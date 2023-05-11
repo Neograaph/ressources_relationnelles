9f5eba9a087513aee637983def4f67a0bd98fc0e
neograaph <max45270@gmail.com>
2023-05-11 14:49:55 +0200

    routing

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAuthentificationComponent } from './Page/page-authentification/page-authentification.component';
+import { PageHomeComponent } from './Page/page-home/page-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "inscription", component: PageAuthentificationComponent },
+  { path: "**", component: PageHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

0016886de0c560fee6e3e6870f60c0e8fd595423
CorentinPihij <pihij.corentin@gmail.com>
2023-05-12 13:44:23 +0200

    Debut page vitrine

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/app.component.html",
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/app/app.component.html", "./src/**/*.{html,ts}"],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'bleufonce': '#092A3F',
-      'blanc': '#F9F9F9',
-      'background-classic': '#DEDEDE',
-      'background-panel': '#EFEFEF',
+      transparent: "transparent",
+      current: "currentColor",
+      bleufonce: "#092A3F",
+      blanc: "#F9F9F9",
+      "background-classic": "#DEDEDE",
+      "background-panel": "#EFEFEF",
+      rouge: "#730217",
+      gris: "#747474",
+      bleubtn: "#0A3A5B",
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-inscription": "url('src/assets/img/bg-inscription.png')",
+        "custom-connexion": "url('src/assets/img/bg-connexion.png')",
+      },
+    },
   },
   plugins: [],
-}
+};

d22cf99d4e089e7922ca56b202199a49a096ff1a
neograaph <max45270@gmail.com>
2023-05-16 14:04:37 +0200

    début .ts

--- Front/src/app/Bloc/bloc-mes-informations/mes-informations.component.ts
+++ Front/src/app/Bloc/bloc-mes-informations/mes-informations.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationsService } from 'src/app/services/notifications.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-mes-informations',
   templateUrl: './mes-informations.component.html',
-  styleUrls: ['./mes-informations.component.css']
+  styleUrls: ['./mes-informations.component.css'],
+  providers: [ToastrService],
+
 })
 export class MesInformationsComponent {
+  profilForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private AuthService: AuthService,
+    private NotificationsService: NotificationsService,
+    private router: Router
+  ) { 
+    this.createForm();
+  }
+
+  OnInit(): void { }
+
+  createForm() {
+    // j'ai mis des valeurs par défaut ici pour les tests pour éviter de remplir le formulaire à chaque fois
+    this.profilForm = this.formBuilder.group({
+      nom: ['neo', Validators.required],
+      prenom: ['graph', Validators.required],
+      email: ['admin@admin.com', [Validators.required, Validators.email]],
+      telephone: ['06 06 06 06 06', [Validators.required]],
+      password: ['azertyui', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['azertyui', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 }
+

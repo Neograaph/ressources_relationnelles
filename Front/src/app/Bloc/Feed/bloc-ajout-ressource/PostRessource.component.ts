e5ba431523316533a23c88825af682fb1debd073
Neograaph <max45270@gmail.com>
2023-05-17 14:24:34 +0200

    fix html

--- Front/src/app/Bloc/Feed/bloc-ajout-ressource/PostRessource.component.ts
+++ Front/src/app/Bloc/Feed/bloc-ajout-ressource/PostRessource.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActionsTypeService } from 'src/app/services/actions-type.service';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-
+import { Ressource } from 'src/app/Models/Ressource.model';
 
 @Component({
   selector: 'app-PostRessource',
@@ -21,6 +20,7 @@ export class PostRessourceComponent implements OnInit {
     this.createForm();
   }
 
+  Ressource?: Ressource;
   ngOnInit(): void {
     // this.actiontype.getActionType().subscribe((response: any) => {
     //   console.log(response)
@@ -28,10 +28,25 @@ export class PostRessourceComponent implements OnInit {
   }
   createForm() {
     // j'ai mis des valeurs par défaut ici pour les tests pour éviter de remplir le formulaire à chaque fois
+    // console.log('test');
     this.ajoutRessourceForm = this.formBuilder.group({
-      titre: ['test', Validators.required],
-      description: ['06 06 06 06 06', [Validators.required]],
+      titre: ['', Validators.required],
+      description: ['', [Validators.required]],
     });
   }
-}
+  publierRessource() {
+    if (this.ajoutRessourceForm.valid) {
+      const formData = this.ajoutRessourceForm.value;
+      console.log(formData);
+      // Envoyer les données du formulaire à l'API ou effectuer toute autre action requise
+      // par exemple, vous pouvez utiliser this.actiontype pour appeler une méthode de votre service
 
+      // Afficher un message de succès
+
+      // Réinitialiser le formulaire après avoir envoyé les données
+      // this.ajoutRessourceForm.reset();
+    } else {
+      // Afficher un message d'erreur si le formulaire est invalide
+    }
+  }
+}

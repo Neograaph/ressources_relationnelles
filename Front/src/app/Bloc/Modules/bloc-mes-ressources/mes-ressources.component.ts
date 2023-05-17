25c8d1f17675fb29011a4d80630d3cd6436de364
Neograaph <max45270@gmail.com>
2023-05-17 15:36:20 +0200

    affichage des ressources

--- Front/src/app/Bloc/Modules/bloc-mes-ressources/mes-ressources.component.ts
+++ Front/src/app/Bloc/Modules/bloc-mes-ressources/mes-ressources.component.ts
@@ -1,36 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ActionsTypeService } from 'src/app/services/actions-type.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ressource } from 'src/app/Models/Ressource.model';
+import { NotificationsService } from 'src/app/services/notifications.service';
+import { RessourcesService } from '../../../services/ressource.service';
 
 @Component({
-  selector: "app-mes-ressources",
-  templateUrl: "./mes-ressources.component.html",
-  styleUrls: ["./mes-ressources.component.css"],
+  selector: 'app-mes-ressources',
+  templateUrl: './mes-ressources.component.html',
+  styleUrls: ['./mes-ressources.component.css'],
+  providers: [ToastrService],
 })
 export class MesRessourcesComponent {
-  data = [
-    {
-      title: "Produit 1",
-      image: "https://picsum.photos/seed/produit1/300/200",
-      description: "Ceci est la description du produit 1"
-    },
-    {
-      title: "Produit 2",
-      image: "https://picsum.photos/seed/produit2/300/200",
-      description: "Ceci est la description du produit 2"
-    },
-    {
-      title: "Produit 3",
-      image: "https://picsum.photos/seed/produit3/300/200",
-      description: "Ceci est la description du produit 3"
-    },
-    {
-      title: "Produit 4",
-      image: "https://picsum.photos/seed/produit4/300/200",
-      description: "Ceci est la description du produit 4"
-    },
-    {
-      title: "Produit 5",
-      image: "https://picsum.photos/seed/produit5/300/200",
-      description: "Ceci est la description du produit 5"
-    }
-  ];
+  constructor(
+    private NotificationsService: NotificationsService,
+    private RessourcesService: RessourcesService
+  ) {}
+  ressource!: Ressource;
+  Ressources: Array<Ressource> = [];
+  ngOnInit(): void {
+    this.getRessources();
+  }
+  getRessources() {
+    this.RessourcesService.getRessources().subscribe((data) => {
+      this.Ressources = data;
+      console.log(data);
+    });
+  }
 }

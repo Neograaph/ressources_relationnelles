1511a2ee9f82529fc75e135277f545d5cbf4b5f8
Neograaph <max45270@gmail.com>
2023-05-17 13:52:00 +0200

    ajout titre + structure

--- Front/src/app/Bloc/Feed/bloc-ajout-ressource/PostRessource.component.ts
+++ Front/src/app/Bloc/Feed/bloc-ajout-ressource/PostRessource.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionsTypeService } from 'src/app/services/actions-type.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-PostRessource',
   templateUrl: './PostRessource.component.html',
   styleUrls: ['./PostRessource.component.css'],
+  providers: [ToastrService],
 })
 export class PostRessourceComponent implements OnInit {
+  ajoutRessourceForm!: FormGroup;
 
-  constructor(public actiontype: ActionsTypeService) { }
+  constructor(
+    public actiontype: ActionsTypeService,
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm();
+  }
+
   ngOnInit(): void {
     // this.actiontype.getActionType().subscribe((response: any) => {
-
     //   console.log(response)
     // });
   }
-
+  createForm() {
+    // j'ai mis des valeurs par défaut ici pour les tests pour éviter de remplir le formulaire à chaque fois
+    this.ajoutRessourceForm = this.formBuilder.group({
+      titre: ['test', Validators.required],
+      description: ['06 06 06 06 06', [Validators.required]],
+    });
+  }
 }
 

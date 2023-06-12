b4bd115a5b448fb7e1e7dee3e0d1c80ccb61c68b
neograaph <max45270@gmail.com>
2023-06-12 11:34:29 +0200

    data composant parent

--- Front/src/app/Bloc/Feed/bloc-ressources/bloc-ressources.component.ts
+++ Front/src/app/Bloc/Feed/bloc-ressources/bloc-ressources.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { RessourcesService } from 'src/app/services/ressource.service';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
+import { Utilisateur } from 'src/app/Models/Utilisateur.model';
+import { Ressource } from 'src/app/Models/Ressource.model';
 
 @Component({
   selector: 'app-bloc-ressources',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./bloc-ressources.component.css'],
 })
 export class BlocRessourcesComponent {
+  utilisateur?: Utilisateur;
+  ressources?: Ressource[];
   name: string = 'neograph';
+
+  constructor(
+    private RessourcesService: RessourcesService,
+    private utilisateurService: UtilisateurService
+  ) {}
+
+  ngOnInit(): void {
+    this.utilisateurService.getUtilisateur().subscribe(
+      (utilisateur: Utilisateur | null) => {
+        if (utilisateur !== null) {
+          this.utilisateur = utilisateur;
+          console.log(this.utilisateur);
+        }
+      },
+      (error) => {
+        console.error(
+          "Une erreur s'est produite lors de la récupération de l'utilisateur :",
+          error
+        );
+      }
+    );
+    this.RessourcesService.getRessources().subscribe(
+      (ressources: Ressource[]) => {
+        this.ressources = ressources;
+        console.log(this.ressources);
+        // Autres actions à effectuer avec les ressources
+      },
+      (error) => {
+        console.error(
+          "Une erreur s'est produite lors de la récupération des ressources :",
+          error
+        );
+      }
+    );
+  }
 }

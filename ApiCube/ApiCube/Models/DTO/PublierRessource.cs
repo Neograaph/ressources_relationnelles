93b448efad2ad96b7b146bb3b14f33301edc5988
Arthur bergere <artesburgerking@gmail.com>
2023-06-12 16:24:57 +0200

    ajout ressource

---
+++ ApiCube/ApiCube/Models/DTO/PublierRessource.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiCube.Models.DTO
+{
+    public class PublierRessource
+    {
+        public int CategorieId { get; set; }
+        public int TypeRessourceId { get; set; }
+        public string Titre { get; set; }
+        public string Contenu { get; set; }
+        public IFormFile Document { get; set; }
+    }
+}

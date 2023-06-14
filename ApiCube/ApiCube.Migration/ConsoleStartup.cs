855536d6a7200744e543d40e357172bab4a395c4
neograaph <max45270@gmail.com>
2023-06-14 13:50:42 +0200

    clean

---
+++ ApiCube/ApiCube.Migration/ConsoleStartup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ApiCube.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiCube.Migration
+{
+    public class ConsoleStartup
+    {
+        public ConsoleStartup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            //.. for test
+            Console.WriteLine(Configuration.GetConnectionString("containerConnection"));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseMySql(Configuration.GetConnectionString("containerConnection"));
+
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+       
+        }
+    }
+}

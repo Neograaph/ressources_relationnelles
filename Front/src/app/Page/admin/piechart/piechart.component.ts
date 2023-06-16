8fa8ad085a8581a713db0e8232619fc376d8b224
Arthur bergere <artesburgerking@gmail.com>
2023-06-16 11:40:02 +0200

    [FUNC] Ajout de chart sur la page admin. (non connecté à l'api)

---
+++ Front/src/app/Page/admin/piechart/piechart.component.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from "@angular/core";
+import { ChartComponent } from "ng-apexcharts";
+
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+
+
+@Component({
+  selector: 'app-piechart',
+  templateUrl: './piechart.component.html',
+  styleUrls: ['./piechart.component.css']
+})
+export class PiechartComponent {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions> & { responsive: ApexResponsive }& { chart: ApexChart }&  { series: ApexNonAxisChartSeries };
+
+  constructor() {
+    this.chartOptions = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: ["Activité / Jeu à réaliser", "Article", "Carte défi", "Cours au format PDF", "Fiche de lecture"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  }
+}

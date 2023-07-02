33b9d63f4921982da6676bc9a0f48c057c3ffb4d
Neograaph <max45270@gmail.com>
2023-07-03 00:50:42 +0200

    fix

--- ApiCube/ApiCube/Controllers/UtilisateursController.cs
+++ ApiCube/ApiCube/Controllers/UtilisateursController.cs
@@ -74,6 +74,18 @@ namespace ApiCube.Controllers
             return Ok(new { Token = tokenString });
         }
 
+        // OPTIONS: api/Utilisateurs/authenticate
+        [HttpOptions("authenticate")]
+        public IActionResult OptionsAuthenticate()
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", "http://cube-cesi.ddns.net:4200");
+            Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+            return Ok();
+        }
+
+
 
         // POST: api/Utilisateurs
         [HttpPost]
